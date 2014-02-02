20f840b84cfb552d063a92e864c9d8ff0c5ba74c
Zoltan Altfatter <altfatterz@gmail.com>
2014-02-02 23:11:07 +0100

    remove unneeded code

--- src/main/java/com/backbase/progfun/Application.java
+++ src/main/java/com/backbase/progfun/Application.java
@@ -14,15 +14,6 @@ public class Application {
     private static final Logger LOGGER = getLogger(Application.class);
 
     public static void main(String[] args) throws Throwable {
-
-        // port configurations for heroku
-//        String port = System.getenv("PORT");
-//        if (port != null && !port.isEmpty()) {
-//            LOGGER.info("Running on port {}", port);
-//            System.setProperty("server.port", port);
-//        }
-
-
         SpringApplication.run(Application.class, args);
     }
 
