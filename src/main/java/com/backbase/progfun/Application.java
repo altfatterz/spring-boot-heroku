8584f43385ab2752659f3a77cbf699176c27e4ff
Zoltan Altfatter <altfatterz@gmail.com>
2014-02-02 22:28:30 +0100

    using tomcat instead and adding port configuration for heroku

--- src/main/java/com/backbase/progfun/Application.java
+++ src/main/java/com/backbase/progfun/Application.java
@@ -1,15 +1,28 @@
 package com.backbase.progfun;
 
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 @EnableAutoConfiguration
 @ComponentScan
 public class Application {
 
+    private static final Logger LOGGER = getLogger(Application.class);
+
     public static void main(String[] args) throws Throwable {
+
+        // port configurations for heroku
+        String port = System.getenv("PORT");
+        if (port != null && !port.isEmpty()) {
+            LOGGER.info("Running on port {}", port);
+            System.setProperty("server.port", port);
+        }
+
+
         SpringApplication.run(Application.class, args);
     }
 
